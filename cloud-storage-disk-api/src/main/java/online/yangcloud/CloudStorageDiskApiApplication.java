7361838a381f211d96bb9961df1568e91ffd2251
zhuboyang <zhuboyang1996@foxmail.com>
2022-12-12 15:19:10 +0800

    + 初始化后端工程与web端工程

---
+++ cloud-storage-disk-api/src/main/java/online/yangcloud/CloudStorageDiskApiApplication.java
@@ -0,0 +1,13 @@
+package online.yangcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageDiskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStorageDiskApiApplication.class, args);
+    }
+
+}
